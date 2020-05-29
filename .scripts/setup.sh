dee12c4abd0bf06516e2b9d15b48f700eaa4be4d
James Vaughn <james.vaughn@canonical.com>
2020-05-29 12:34:48 +0100

    Convert to generic-ubuntu-desktop branch
    Was previously vagrant-ubuntu-desktop branch

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,35 +1,38 @@
 #!/bin/sh
 
-# Set timezone
+user=$(getent passwd 1000 | cut -d ':' -f 1)
+user_home=$(getent passwd 1000 | cut -d ':' -f 6)
+
 sudo timedatectl set-timezone Europe/London
 
-# Install packages
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get -y install --no-install-recommends ubuntu-desktop virtualbox-guest-dkms
-sudo apt-get -y install apt-file devscripts language-pack-en neovim virt-manager ssh-askpass-gnome zsh
+sudo apt-get -y install --no-install-recommends ubuntu-desktop
+sudo apt-get -y install apt-file devscripts language-pack-en neovim virt-manager virt-what ssh-askpass-gnome zsh
+if [ "$(sudo virt-what | head -n 1)" = 'virtualbox' ]; then
+	sudo apt-get -y install virtualbox-guest-dkms
+fi
 
-# Set locale/language to English (United Kingdom)
 sudo update-locale LANG=en_GB.UTF.8
 
-# Configure dotfiles
-sudo -u vagrant git clone --bare https://github.com/jmcvaughn/dotfiles.git /home/vagrant/.dotfiles/
-sudo -u vagrant git --git-dir=/home/vagrant/.dotfiles/ --work-tree=/home/vagrant/ config core.sparseCheckout true
-printf '/*\n!/README.md\n' | sudo -u vagrant tee /home/vagrant/.dotfiles/info/sparse-checkout
-sudo -u vagrant git --git-dir=/home/vagrant/.dotfiles/ --work-tree=/home/vagrant/ checkout vagrant-ubuntu-desktop
-sudo -u vagrant git --git-dir=/home/vagrant/.dotfiles/ --work-tree=/home/vagrant/ submodule update --init
+# Clone dotfiles
+sudo -u "$user" git clone --bare https://github.com/jmcvaughn/dotfiles.git "$user_home"/.dotfiles/
+sudo -u "$user" git --git-dir="$user_home"/.dotfiles/ --work-tree="$user_home"/ config core.sparseCheckout true
+printf '/*\n!/README.md\n' | sudo -u "$user" tee "$user_home"/.dotfiles/info/sparse-checkout
+sudo -u "$user" git --git-dir="$user_home"/.dotfiles/ --work-tree="$user_home"/ checkout generic-ubuntu-desktop
+sudo -u "$user" git --git-dir="$user_home"/.dotfiles/ --work-tree="$user_home"/ submodule update --init
 
 # Set zsh as default shell
-sudo chsh -s /bin/zsh vagrant
+sudo chsh -s /bin/zsh "$user"
 
 # Generate SSH key pair
-sudo -u vagrant ssh-keygen -b 4096 -t rsa -N '' -f /home/vagrant/.ssh/id_rsa
+sudo -u "$user" ssh-keygen -b 4096 -t rsa -N '' -f "$user_home"/.ssh/id_rsa
 
-# Configure autologin
-cat << 'EOF' | sudo tee /etc/gdm3/custom.conf
+# Configure auto-login
+cat << EOF | sudo tee /etc/gdm3/custom.conf
 [daemon]
 AutomaticLoginEnable = true
-AutomaticLogin = vagrant
+AutomaticLogin = $user
 EOF
 
 sudo reboot
