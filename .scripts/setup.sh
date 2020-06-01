990798481bb4c8e7b767827992fe8039319dd612
James Vaughn <james.vaughn@canonical.com>
2020-06-01 15:59:58 +0100

    Fix locale

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -17,7 +17,7 @@ libvirt_pkgs='libvirt-clients libvirt-daemon-system'
 [ "$DISTRIB_RELEASE" = '16.04' ] && libvirt_pkgs='libvirt-bin'
 sudo apt-get -y install apt-file aria2 bridge-utils devscripts jq language-pack-en $libvirt_pkgs neovim nfs-common qemu-kvm source-highlight tree zip zsh
 
-sudo update-locale LANG=en_GB.UTF.8
+sudo update-locale LANG=en_GB.UTF-8
 
 # Clone dotfiles
 sudo -u "$user" git clone --bare https://github.com/jmcvaughn/dotfiles.git "$user_home"/.dotfiles/
