59422b7e3a523260fc6c5d33eacefdbe2e5ad01b
James Vaughn <james.vaughn@canonical.com>
2021-02-15 17:25:57 +0000

    Remove Vagrant/cloud compatibility, use bash in setup.sh
    Converting a server installation to desktop doesn't seem to work well at
    all (performance issues, the installation isn't the same), so abandon
    this capability. It is now presumed that this will be run in a complete
    desktop installation.
    
    Correct #! line in setup script to use bash explicitly. bashisms are
    used but was using POSIX sh. This worked but is bad practice.

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set variables
 . /etc/lsb-release
-user=$(getent passwd 1000 | cut -d ':' -f 1)
-user_home=$(getent passwd 1000 | cut -d ':' -f 6)
 
 sudo timedatectl set-timezone Europe/London
 
@@ -20,7 +18,6 @@ if [ "${DISTRIB_RELEASE%.*}" -le 18 ]; then
 fi
 
 sudo apt-get update
-sudo apt-get -y install --no-install-recommends ubuntu-desktop
 [ "${DISTRIB_RELEASE%.*}" -ge 20 ] && yarn_pkg='yarnpkg' || yarn_pkg='yarn'
 sudo apt-get -y install apt-file default-jre-headless devscripts jq language-pack-en neovim nfs-common nodejs ssh-askpass-gnome tree virt-manager virt-what "$yarn_pkg" zip zsh
 hypervisor=$(sudo virt-what | head -n 1)
@@ -33,32 +30,16 @@ sudo snap install batcat --classic
 sudo update-locale LANG=en_GB.UTF-8
 
 # Clone dotfiles
-sudo -u "$user" git clone --bare https://github.com/jmcvaughn/dotfiles.git "$user_home"/.dotfiles/
-sudo -u "$user" git --git-dir="$user_home"/.dotfiles/ --work-tree="$user_home"/ config core.sparseCheckout true
-printf '/*\n!/README.md\n' | sudo -u "$user" tee "$user_home"/.dotfiles/info/sparse-checkout
-sudo -u "$user" git --git-dir="$user_home"/.dotfiles/ --work-tree="$user_home"/ checkout generic-ubuntu-desktop
-## Login shell (`-i`) required, otherwise the following error occurs:
-## fatal: /usr/lib/git-core/git-submodule cannot be used without a working tree.
-## Vagrant runs scripts as UID 1000 with sudo so it doesn't require `-i`
-sudo -iu "$user" git --git-dir="$user_home"/.dotfiles/ --work-tree="$user_home"/ submodule update --init --recursive --jobs 4
+git clone --bare https://github.com/jmcvaughn/dotfiles.git "$HOME"/.dotfiles/
+git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" config core.sparseCheckout true
+printf '/*\n!/README.md\n' | tee "$HOME"/.dotfiles/info/sparse-checkout
+git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" checkout generic-ubuntu-desktop
+git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" submodule update --init --recursive --jobs 4
 
 # Set zsh as default shell
-sudo chsh -s /bin/zsh "$user"
-
-# Add this user to the same groups as "ubuntu" user, if it exists
-ubuntu_groups=$(groups ubuntu | tr ' ' ',' | cut -d ',' -f '4-') 2> /dev/null
-if [ "$user" != 'ubuntu' ] && [ -n "$ubuntu_groups" ]; then
-	sudo usermod --append --groups $ubuntu_groups "$user"
-fi
+[[ "$SHELL" != *'zsh' ]] && chsh -s /bin/zsh
 
 # Generate SSH key pair
-sudo -u "$user" ssh-keygen -b 4096 -t rsa -N '' -f "$user_home"/.ssh/id_rsa
-
-# Configure auto-login
-cat << EOF | sudo tee /etc/gdm3/custom.conf
-[daemon]
-AutomaticLoginEnable = true
-AutomaticLogin = $user
-EOF
+ssh-keygen -b 4096 -t rsa -N '' -f "$HOME"/.ssh/id_rsa
 
 echo 'Optional: run ~/.scripts/nvim.sh to generate helptags'
