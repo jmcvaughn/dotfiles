4556debaadd91e19873bf70ac2b726e1090366a9
James Vaughn <james.vaughn@canonical.com>
2021-05-24 17:56:36 +0100

    Add markdown-preview.nvim

--- bin/repolists.py
+++ bin/repolists.py
@@ -4,9 +4,13 @@
 # yarn: list of packages
 # zsh: list of 2-tuples of URL and branch
 git = [
-        ('https://github.com/bswinnerton/launchbar-github', 'master',
+    ('https://github.com/bswinnerton/launchbar-github', 'master',
         '~/Library/Application Support/LaunchBar/Actions/github.lbaction'),
-        ('https://github.com/jmcvaughn/less_app', 'master', '~/git/less_app/')
+    ('https://github.com/jmcvaughn/less_app', 'master', '~/git/less_app/')
+]
+
+neovim = [
+    ('https://github.com/iamcco/markdown-preview.nvim', 'master', True),
 ]
 
 zsh = [
