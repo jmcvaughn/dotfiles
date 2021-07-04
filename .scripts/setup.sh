9aeeb275226a9438ea7949a97a2429cb12e070e1
James Vaughn <james.vaughn@canonical.com>
2021-07-04 13:18:08 +0100

    Add kubectl snap
    stsstack-bundles automatically installs this, but just to be thorough
    (e.g. if stsstack-bundles isn't used).

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -70,7 +70,7 @@ sudo apt-get update
 sudo apt-get -y install --no-install-recommends gnuplot virtinst
 sudo apt-get -y install ${packages[@]}
 sudo snap install canonical-livepatch cmadison hotsos maas maas-test-db openstackclients vault
-for i in batcat charm juju; do
+for i in batcat charm juju kubectl; do
 	sudo snap install "$i" --classic
 done
 
