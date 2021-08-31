054afa968fa2ffaba171699da6c7d25fe074fc1a
James Vaughn <james.vaughn@canonical.com>
2021-08-31 12:49:58 +0100

    Add GitHub CLI (gh)

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -11,6 +11,7 @@ packages=(
 	docker-compose
 	fio
 	genisoimage
+	gh
 	git-review
 	iotop
 	ipmitool
@@ -66,6 +67,9 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 ## HashiCorp
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+## GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 # Install packages
 sudo apt-get update
