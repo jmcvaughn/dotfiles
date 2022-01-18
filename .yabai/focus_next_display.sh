6c6e4b81c5283a6952cc9c872b6c76d0e601a20e
James Vaughn <james.vaughn@canonical.com>
2022-01-18 12:14:55 +0000

    WM: Update scripts for yabai 4

--- .yabai/focus_next_display.sh
+++ .yabai/focus_next_display.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 num_displays=$(yabai -m query --displays | jq '.[-1].index')
-focused=$(yabai -m query --windows | jq '.[] | select(.focused==1) | .display')
+focused=$(yabai -m query --windows | jq '.[] | select(."has-focus" == true) | .display')
 
 if [ "$focused" = "$num_displays" ]; then
 	yabai -m display --focus 1
