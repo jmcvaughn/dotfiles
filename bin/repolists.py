ebb04cfde4a4146bc34845fb2e536a8d37044a16
James Vaughn <james.vaughn@canonical.com>
2022-05-29 11:35:04 +0100

    Fix repolists indentation

--- bin/repolists.py
+++ bin/repolists.py
@@ -2,17 +2,16 @@
 # neovim: list of 3-tuples of url, branch, and whether `yarn install` required
 # zsh: list of 2-tuples of URL and branch
 neovim = [
-        ('https://github.com/morhetz/gruvbox', 'master', False),
-        ('https://github.com/tomtom/tcomment_vim', 'master', False),
-        ('https://github.com/rbong/vim-crystalline', 'master', False),
-        ('https://github.com/tpope/vim-fugitive', 'master', False),
-        ('https://github.com/tpope/vim-unimpaired', 'master', False)
+    ('https://github.com/morhetz/gruvbox', 'master', False),
+    ('https://github.com/tomtom/tcomment_vim', 'master', False),
+    ('https://github.com/rbong/vim-crystalline', 'master', False),
+    ('https://github.com/tpope/vim-fugitive', 'master', False),
+    ('https://github.com/tpope/vim-unimpaired', 'master', False)
 ]
 
 zsh = [
-        ('https://github.com/zsh-users/zsh-autosuggestions', 'master'),
-        ('https://github.com/zsh-users/zsh-completions', 'master'),
-        ('https://github.com/zsh-users/zsh-history-substring-search',
-            'master'),
-        ('https://github.com/zsh-users/zsh-syntax-highlighting', 'master')
+    ('https://github.com/zsh-users/zsh-autosuggestions', 'master'),
+    ('https://github.com/zsh-users/zsh-completions', 'master'),
+    ('https://github.com/zsh-users/zsh-history-substring-search', 'master'),
+    ('https://github.com/zsh-users/zsh-syntax-highlighting', 'master')
 ]
