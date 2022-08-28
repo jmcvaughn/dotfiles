767e4fcfe934585529c3fd44b3f52140656869ee
James Vaughn <james.vaughn@canonical.com>
2022-08-28 13:12:09 +0100

    Transition Neovim markdown-preview configuration to Lua

---
+++ .config/nvim/30-markdown-preview.nvim.lua
@@ -0,0 +1,6 @@
+-- https://github.com/iamcco/markdown-preview.nvim
+
+-- set to 1, the nvim will auto close current preview window when change
+-- from markdown buffer to another buffer
+-- default: 1
+vim.g.mkdp_auto_close = 0
