216f37eb9ece265f8d140ce5f7c46034c33d2f7d
James Vaughn <james.vaughn@weka.io>
2024-04-11 12:53:29 +0100

    WM: Automatically move chat apps to second window, use Bash explicitly

--- .yabai/float_default_size.sh
+++ .yabai/float_default_size.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ratio=$(($(yabai -m query --displays --display | jq -r '.frame | del(.x, .y) | join("/")')))
 
@@ -6,5 +6,3 @@ case $ratio in
 	1) yabai -m window --grid 10:10:1:1:8:7 ;;  # Normal aspect ratios, e.g. 16:10
 	2) yabai -m window --grid 10:10:3:1:4:6 ;;  # Ultrawide, e.g. 21:9
 esac
-
-# vim: set filetype=bash:
