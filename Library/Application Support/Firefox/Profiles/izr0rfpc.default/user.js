5c0c649a240b5bb371f5eb937e70587e5882afb3
James Vaughn <24993138+jmcvaughn@users.noreply.github.com>
2026-05-18 13:56:47 +0100

    Update Firefox configuration, unmanage handlers.json

--- Library/Application Support/Firefox/Profiles/izr0rfpc.default/user.js
+++ Library/Application Support/Firefox/Profiles/izr0rfpc.default/user.js
@@ -8,17 +8,13 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 // General > Ctrl+Tab cycles through tabs in recently used order: False
 user_pref("browser.ctrlTab.recentlyUsedOrder", false);
 
-// General > Confirm before quitting with Command+Q: False
+// General > Ask before quitting with Command+Q: False
 user_pref("browser.warnOnQuitShortcut", false);
 
-// General > Allow Firefox to: Check for updates but let you choose to install
-// them
-user_pref("app.update.auto", false);
-
-// General > Recommend extensions as you browser: False
+// General > Recommend extensions as you browse: False
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 
-// General > Recommend features as you browser: False
+// General > Recommend features as you browse: False
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 
 // Home > Homepage and new windows: Blank Page
@@ -32,26 +28,14 @@ user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
 
-// Privacy & Security > Ask to save logins and passwords for web sites: False
+// Privacy & Security > Ask to save passwords: False
 user_pref("signon.rememberSignons", false);
 
 // Privacy & Security > Clear History > Time range to clear: Everything
 user_pref("privacy.sanitize.timeSpan", 0);
 
-// Privacy & Security > Allow Firefox to send technical and interaction data to Mozilla: False
+// Privacy & Security > Send technical and interaction data to Mozilla: False
 user_pref("datareporting.healthreport.uploadEnabled", false);
 
-// Privacy & Security > Allow Firefox to install and run studies: False
-user_pref("app.shield.optoutstudies.enabled", false);
-
-// Privacy & Security > Clear history when Firefox closes > Settings > Browsing History: False
+// Privacy & Security > History > Remember history
 user_pref("privacy.clearOnShutdown.history", false);
-
-
-/******************************************************************************
- * about:config                                                               *
- ******************************************************************************/
-
-// Disable Pocket
-// https://support.mozilla.org/en-US/kb/disable-or-re-enable-pocket-for-firefox
-user_pref("extensions.pocket.enabled", false);
